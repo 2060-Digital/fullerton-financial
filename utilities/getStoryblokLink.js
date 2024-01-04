72221cc36bf327db5fdc19abb8a68829f86bd873
Abel Marin <abel.marin@2060digital.com>
2024-01-04 15:10:31 -0500

    Update storyblok link functions

--- utilities/getStoryblokLink.js
+++ utilities/getStoryblokLink.js
@@ -1,15 +1,38 @@
-export const getStoryblokLink = ({ href, anchor, linktype, cached_url, cachedUrl, url, email } = {}) => {
+export const getStoryblokLink = ({
+  href,
+  anchor,
+  linktype,
+  cached_url,
+  cachedUrl,
+  url,
+  email,
+  filename,
+  fieldtype,
+} = {}) => {
+  if (fieldtype === "asset") return filename
   if (linktype === "email") return `mailto:${email || url || href}`
 
+  // inline links
+  // if a link is inline within a rich text field then an href will be provided
+  if (href !== null && href !== undefined && href !== "") return href
+
   // internal link
-  if (linktype === "story" && cached_url !== "") {
+  // cached_url can be undefined if an internal link field is left blank
+  if (linktype === "story" && cached_url !== "" && cached_url !== undefined) {
     return anchor ? `/${cached_url}#${anchor}` : `/${cached_url}`
   }
 
   // Don't ask me why, but Storyblok is inconsistent with this property's case.
   if (cached_url || cachedUrl || href) {
-    return cached_url ?? cachedUrl ?? href
+    return cached_url ?? cachedUrl ?? relativizeToBase(href)
   }
 
   return null
+}
+
+const isRelative = (str) => !/^(?:[a-z+]+:)?\/\//.test(str)
+
+// find only scheme/protocol-free relative URLs without forward slash and add them.
+const relativizeToBase = (href) => {
+  return href && isRelative(href) && (href.startsWith("/") ? href : `/${href}`)
 }
