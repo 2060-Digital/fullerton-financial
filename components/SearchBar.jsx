0cf443c7c90f909afa5b793b7a9f32ebf8964a7b
Abel Marin <abel.marin@2060digital.com>
2024-01-11 15:48:02 -0500

    Add dark style to searchbar

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,7 +1,14 @@
-export default function SearchBar() {
+export default function SearchBar({ style = "dark" }) {
   return (
-    <form className="searchbar w-full relative max-w-80">
-      <input type="text" id="search" name="search" placeholder="Search Our Site" maxLength={50} />
+    <form className="searchbar w-full relative sm:max-w-80">
+      <input
+        type="text"
+        id="search"
+        name="search"
+        className={`${style}-searchbar-input`}
+        placeholder="Search Our Site"
+        maxLength={50}
+      />
     </form>
   )
 }
