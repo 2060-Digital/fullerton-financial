9a394186b8c9768862afdc11cef3d7ed1ad1a73a
Abel Marin <abel.marin@2060digital.com>
2024-01-16 17:53:58 -0500

    Style image and content

--- components/DynamicComponent/molecules/ImageAndContent.jsx
+++ components/DynamicComponent/molecules/ImageAndContent.jsx
@@ -1,44 +1,66 @@
+import cn from "classnames"
 import Image from "components/Image"
 import richText from "utilities/richText"
-import cn from "classnames"
+import DynamicComponent from "components/DynamicComponent"
 
 export default function ImageAndContent({ blok }) {
-  const section = cn(blok.background_color && `bg-${blok.background_color}`)
-
-  const container = cn(
-    "image-content-container flex flex-col gap-4 md:gap-12 items-center md:items-start px-10 xl:px-8 mx-auto max-w-screen-xl",
-    {
-      "md:flex-row-reverse justify-end": blok.orientation === "content_first",
-      "md:flex-row": blok.orientation === "image_first",
-    }
-  )
-
-  const imgDecorContainer = cn("image-decor max-w-lg md:max-w-none md:w-full basis-1/2 relative pb-20 md:pb-12")
-
-  const image = cn("relative z-10", {
-    "pl-12 xl:pl-40": blok.orientation === "content_first",
-    "pr-12": blok.orientation === "image_first",
-  })
-
   return (
-    <section className={section}>
-      <div className={container}>
-        <div className={imgDecorContainer}>
-          <div className={image}>
-            <Image
-              src={blok.image.filename}
-              alt={blok.image.alt}
-              placeholder={blok.image?.blurDataURL ? "blur" : "empty"}
-              blurDataURL={blok.image?.blurDataURL}
-              width={435}
-              height={511}
-            />
+    <section
+      className={cn("lg:px-6 lg:my-20", {
+        "prose-headings:text-white prose-p:text-white": blok?.background_color === "primary-1",
+        "prose-headings:text-primary-1": blok?.background_color === "secondary-1" || blok?.background_color === "gray",
+      })}
+    >
+      <div
+        className={cn(
+          blok?.background_color && `bg-${blok?.background_color}`,
+          "px-6 lg:px-24 py-12 lg:py-20 mx-auto max-w-screen-xl",
+          {
+            "bg-opacity-20": blok?.background_color === "secondary-1",
+            "lg:border-l-[80px] lg:border-l-white": blok?.orientation === "image_first",
+            "lg:border-r-[80px] lg:border-r-white": blok?.orientation === "content_first",
+          },
+        )}
+      >
+        <div
+          className={cn("flex flex-col gap-8 lg:gap-0 items-center mx-auto max-w-screen-xl", {
+            "lg:flex-row-reverse justify-end": blok?.orientation === "image_first",
+            "lg:flex-row": blok?.orientation === "content_first",
+          })}
+        >
+          <div className="lg:basis-5/12">
+            <h2>
+              {blok?.eyebrow ? <span className="block eyebrow pb-2.5">{blok?.eyebrow}</span> : null}
+              <span className="pb-5 block">{blok?.heading}</span>
+            </h2>
+            <div className="">{richText(blok?.content)}</div>
+          </div>
+          <div
+            className={cn("w-full h-full lg:basis-7/12 relative pr-4 pt-4 lg:scale-110", {
+              "lg:-left-32": blok?.orientation === "image_first",
+              "lg:-right-32": blok?.orientation === "content_first",
+            })}
+          >
+            <div className="relative z-10 w-full h-full border-2 border-secondary-1">
+              <Image
+                src={blok?.image?.filename}
+                alt={blok?.image?.alt}
+                placeholder={blok?.image?.blurDataURL ? "blur" : "empty"}
+                blurDataURL={blok?.image?.blurDataURL}
+                width={435}
+                height={511}
+                className="w-full relative -top-4 -right-4"
+              />
+            </div>
           </div>
         </div>
-        <div className="basis-1/2 image-content">
-          <h2>{blok.heading}</h2>
-          <div className="leading-8 prose-p:py-2">{richText(blok.content)}</div>
-        </div>
+        {blok?.nested_bloks?.length ? (
+          <>
+            {blok?.nested_bloks?.map((blok) => (
+              <DynamicComponent blok={blok} key={blok._uid} />
+            ))}
+          </>
+        ) : null}
       </div>
     </section>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   mode: "jit",
+  safelist: ["bg-white", "bg-primary-1", "bg-highlight-tint", "bg-gray", "bg-secondary-1"],
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
