1f4815d239083130058798042ccbf1e875c4bad4
Abel Marin <abel.marin@2060digital.com>
2024-02-01 12:47:14 -0500

    Add background image field

--- components/DynamicComponent/atoms/CTABanner.jsx
+++ components/DynamicComponent/atoms/CTABanner.jsx
@@ -11,11 +11,15 @@ export default function CTABanner({
     },
     label: "Schedule a Meeting",
     heading: "Ready to get started?",
+    background_image: { filename: "https://a-us.storyblok.com/f/1017266/1440x283/9383e37db4/fullerton-cta-bar.webp" },
   },
 }) {
   return (
     <aside className="cta-banner-section">
-      <div className="py-12 lg:py-16 px-6 bg-cta-banner bg-cover bg-no-repeat">
+      <div
+        className="py-12 lg:py-16 px-6 bg-cover bg-no-repeat"
+        style={{ backgroundImage: `url(${blok?.background_image?.filename})` }}
+      >
         <div className="max-w-screen-xl mx-auto flex flex-col gap-8 items-center">
           <h2 className="text-center text-primary-2 text-xl1 lg:text-xl2">{blok.heading}</h2>
           <CallToAction href={getStoryblokLink(blok.link)}>{blok.label}</CallToAction>
--- components/DynamicComponent/molecules/SidebarCTA.jsx
+++ components/DynamicComponent/molecules/SidebarCTA.jsx
@@ -9,7 +9,10 @@ export default function SidebarCTA({ blok }) {
         <div className="lg:basis-2/3 prose-h2:text-primary-1 prose-headings:pb-6 px-6 lg:px-0 pb-8 lg:pb-0 w-full">
           {richText(blok.content)}
         </div>
-        <aside className="lg:basis-1/3 py-12 px-6 bg-cta-banner bg-cover bg-no-repeat lg:sticky lg:top-8">
+        <aside
+          className="lg:basis-1/3 py-12 px-6 bg-cover bg-no-repeat bg-center lg:sticky lg:top-8"
+          style={{ backgroundImage: `url(${blok?.sidebar_background?.filename})` }}
+        >
           <div className="max-w-screen-xl mx-auto flex flex-col gap-8 items-center">
             <h2 className="text-center text-primary-2 text-xl1">{blok.sidebar_heading}</h2>
             <CallToAction href={getStoryblokLink(blok.sidebar_link)}>{blok.sidebar_link_label}</CallToAction>
