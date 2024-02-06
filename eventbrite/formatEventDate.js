7e5a1df6bb69a69113372d71128457d5b9d4ae4d
Abel Marin <abel.marin@2060digital.com>
2024-02-06 17:39:02 -0500

    Add suffixes to date formatter

--- eventbrite/formatEventDate.js
+++ eventbrite/formatEventDate.js
@@ -1,5 +1,17 @@
-export const formatEventDate = (str) =>
-  new Intl.DateTimeFormat("en-US", { weekday: "long", month: "long", day: "numeric" }).format(new Date(str))
+export const formatEventDate = (str) => {
+  const date = new Date(str)
+
+  const day = new Intl.PluralRules("en-US", { type: "ordinal" }).select(date.getDate())
+
+  const suffixes = {
+    one: "st",
+    two: "nd",
+    few: "rd",
+    other: "th",
+  }
+
+  return `${new Intl.DateTimeFormat("en-US", { weekday: "long", month: "long", day: "numeric" }).format(date)}${suffixes[day]}`
+}
 
 export const formartEventTime = (time) => {
   return new Intl.DateTimeFormat("en-US", { hour: "numeric", minute: "numeric" }).format(new Date(time))
