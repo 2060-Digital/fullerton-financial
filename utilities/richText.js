d5c51fed833fe116977f6f556fb59601c1cda587
Ben McGrath <brmc14@gmail.com>
2024-02-14 13:02:49 -0500

    add slugify, update richText util

--- utilities/richText.js
+++ utilities/richText.js
@@ -1,9 +1,18 @@
-import { render, MARK_LINK, MARK_TEXT_STYLE, NODE_HEADING, NODE_UL, NODE_OL } from "storyblok-rich-text-react-renderer"
+import {
+  render,
+  MARK_LINK,
+  NODE_HEADING,
+  MARK_TEXT_STYLE,
+  MARK_UNDERLINE,
+  NODE_UL,
+  NODE_OL,
+} from "storyblok-rich-text-react-renderer"
 import CallToAction from "components/CallToAction"
 import cn from "classnames"
 import { Components } from "components/DynamicComponent"
 import { getStoryblokLink } from "./getStoryblokLink"
 import React from "react"
+import slugify from "slugify"
 
 export default function richText(content) {
   const blokResolvers = Object.keys(Components).reduce((blokResolvers, name) => {
@@ -35,11 +44,31 @@ export default function richText(content) {
             const Tag = ["h1", "h2", "h3", "h4", "h5", "h6"]
             const Component = Tag[level - 1]
             if (children && typeof children[0] === "string") {
-              return <Component>{children}</Component>
+              return (
+                <Component
+                  id={slugify(children.join(), {
+                    lower: true,
+                  })}
+                  className="rich-text-heading"
+                >
+                  {children}
+                </Component>
+              )
             }
 
             return children ? (
-              <Component className={cn(children[0].props.className)}>{children[0].props.children}</Component>
+              <Component
+                id={
+                  children?.length > 0 && typeof children[0]?.props?.children === "string"
+                    ? slugify(children[0]?.props?.children, {
+                        lower: true,
+                      })
+                    : undefined
+                }
+                className={cn(children[0].props.className, "rich-text-heading")}
+              >
+                {children[0].props.children}
+              </Component>
             ) : null
           },
           [NODE_UL]: (children) => {
@@ -60,6 +89,10 @@ export default function richText(content) {
           // Prevents content editors from setting a custom text color
           [MARK_TEXT_STYLE]: (children) => {
             return children
+          },
+
+          [MARK_UNDERLINE]: (children) => {
+            return <span className="rich-text-underline font-secondary">{children}</span>
           },
         },
       })
