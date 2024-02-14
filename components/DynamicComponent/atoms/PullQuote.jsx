00fda702dce1478a117e1f6c507279897f3f073e
Abel Marin <abel.marin@2060digital.com>
2024-02-14 17:51:56 -0500

    Fix pull quote text size

--- components/DynamicComponent/atoms/PullQuote.jsx
+++ components/DynamicComponent/atoms/PullQuote.jsx
@@ -1,7 +1,7 @@
 export default function PullQuote({ blok }) {
   return (
     <div className="p-7 mb-4 bg-secondary-2">
-      <blockquote className="font-secondary text-primary-1 font-medium leading-7 lg:text-center">
+      <blockquote className="font-secondary text-primary-1 font-medium leading-7 lg:text-center text-m2">
         {blok.quote}
       </blockquote>
       {blok?.citation?.length ? (
