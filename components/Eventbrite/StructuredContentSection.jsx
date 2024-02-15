5e27d3442353b30a2652846e6561a751a79b9616
Abel Marin <abel.marin@2060digital.com>
2024-02-15 12:41:04 -0500

    Fix structured content keys

--- components/Eventbrite/StructuredContentSection.jsx
+++ components/Eventbrite/StructuredContentSection.jsx
@@ -1,13 +1,13 @@
-import Image from "components/Image"
+import Image from "next/image"
 
 export default function StructuredContentSection({ modules }) {
   return modules?.length > 0 ? (
     <section className="px-6">
       <div className="py-12 lg:py-24 grid lg:grid-cols-2 max-w-screen-xl mx-auto gap-12 border-b-2 border-b-secondary-1">
-        {modules.map(({ data }) => {
+        {modules.map(({ data, id }) => {
           if (data?.image) {
             return (
-              <div className="relative z-10 w-full h-full border-2 border-secondary-1 mt-4 mr-4" key={data?.image?.id}>
+              <div className="relative z-10 w-full h-full border-2 border-secondary-1 mt-4 mr-4" key={id}>
                 <Image
                   src={data?.image?.url}
                   alt=""
@@ -19,10 +19,11 @@ export default function StructuredContentSection({ modules }) {
               </div>
             )
           }
+
           return (
             <div
               dangerouslySetInnerHTML={{ __html: data?.body?.text ?? null }}
-              key={data?.body?.id}
+              key={id}
               className="event-page-content prose-headings:text-primary-1 prose-headings:pb-4 prose-h3:text-l2 prose-h3:lg:text-xl1 prose-h3:pt-2 first:prose-h3:pt-0 prose-h4:text-m2 prose-h4:lg:text-l1 prose-h4:font-secondary"
             ></div>
           )
