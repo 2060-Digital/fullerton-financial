7de13627f7a3281efd24cf2c0bdbecb904185b45
Ben McGrath <brmc14@gmail.com>
2024-02-19 17:53:32 -0500

    add content to side layout, adding two column layout

--- components/DynamicComponent/molecules/FormEmbedSection.jsx
+++ components/DynamicComponent/molecules/FormEmbedSection.jsx
@@ -7,13 +7,14 @@ export default function FormEmbedSection({ blok }) {
     <section className={`px-5 py-8 lg:py-16 relative`}>
       <div
         className={cn("relative max-w-screen-xl mx-auto", {
-          "max-w-5xl": blok.content_placement === "top",
-          "flex flex-col lg:flex-row gap-7": blok.content_placement === "side",
+          "max-w-5xl w-full": blok.content_placement === "top",
+          "flex flex-col lg:flex-row lg:gap-7": blok.content_placement === "side",
         })}
       >
         <div
           className={cn("pb-8 lg:pb-16 text-primary prose-headings:text-primary-1 prose-headings:pb-7", {
             "text-center": blok.content_placement === "top",
+            "lg:basis-1/3": blok.content_placement === "side",
           })}
         >
           {richText(blok?.intro_content)}
@@ -21,7 +22,7 @@ export default function FormEmbedSection({ blok }) {
         <div
           className={cn({
             "max-w-4xl mx-auto": blok.content_placement === "top",
-            "w-full": blok.content_placement === "side",
+            "w-full lg:basis-2/3": blok.content_placement === "side",
           })}
         >
           <WufooEmbed formID={blok?.form_id} initialHeight={blok.initial_height} />
--- components/WufooEmbed.jsx
+++ components/WufooEmbed.jsx
@@ -1,19 +1,16 @@
-export default function WufooEmbed({ formID, initialHeight = 640 }) {
-  const height = parseInt(initialHeight) + 300
+import Script from "next/script"
 
+export default function WufooEmbed({ formID, initialHeight = 640 }) {
   return (
-    <div>
-      <iframe
-        height={height}
-        title="Embedded Wufoo Form"
-        allowTransparency
-        frameborder="0"
-        style={{ width: "100%", border: "none" }}
-        src={`https://fullertonfinancial.wufoo.com/embed/${formID}/`}
-        sandbox="allow-popups-to-escape-sandbox allow-top-navigation allow-scripts allow-popups allow-forms allow-same-origin"
-        allowFullScreen
-        allow="geolocation; microphone; camera"
-      />
-    </div>
+    <>
+      <div id={`wufoo-${formID}`}>
+        {" "}
+        Please fill out my <a href={`https://fullertonfinancial.wufoo.com/forms/${formID}`}>online form</a>.{" "}
+      </div>{" "}
+      <Script strategy="afterInteractive" id="wufoo_form_embed">
+        {`var ${formID} 
+      (function(d, t) { var s = d.createElement(t), options = { 'userName':'fullertonfinancial', 'formHash':'${formID}', 'autoResize':true, 'height':'${initialHeight}', 'async':true, 'host':'wufoo.com', 'header':'show', 'ssl':true }; s.src = ('https:' == d.location.protocol ?'https://':'http://') + 'secure.wufoo.com/scripts/embed/form.js'; s.onload = s.onreadystatechange = function() { var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return; try { ${formID} = new WufooForm(); ${formID}.initialize(options); ${formID}.display(); } catch (e) { } }; var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr); })(document, 'script');`}
+      </Script>
+    </>
   )
 }
