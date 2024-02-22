ccd4657557aaa86a54e018a262ff335764cdc6bf
Ben McGrath <brmc14@gmail.com>
2024-02-22 14:59:09 -0500

    style responsive grid

--- components/DynamicComponent/molecules/LogoGridSection.jsx
+++ components/DynamicComponent/molecules/LogoGridSection.jsx
@@ -1,4 +1,9 @@
 import richText from "utilities/richText"
+import Link from "next/link"
+import Image from "next/image"
+import { getStoryblokLink } from "utilities/getStoryblokLink"
+import getSbImageDimensions from "utilities/getImageDimensions.ts"
+getSbImageDimensions
 
 export default function LogoGridSection({ blok }) {
   return (
@@ -8,7 +13,25 @@ export default function LogoGridSection({ blok }) {
           {blok?.eyebrow ? <span className="text-primary-1 block eyebrow pb-2.5">{blok?.eyebrow}</span> : null}
           <span className="pb-5 block text-primary-1">{blok?.heading}</span>
         </h2>
-        <div className="max-w-4xl mx-auto">{richText(blok.content)}</div>
+        <div className="max-w-4xl mx-auto pb-8">{richText(blok.content)}</div>
+        <div className="grid grid-cols-2 gap-4 lg:gap-16 md:grid-cols-3 lg:grid-cols-4 justify-between">
+          {blok?.logos?.map((logo) => {
+            return (
+              <Link key={logo._uid} href={getStoryblokLink(logo.url)}>
+                <div className="border border-secondary-2 min-h-[108px] lg:min-h-[181px] p-4 h-full flex items-center justify-center">
+                  {" "}
+                  <Image
+                    className=""
+                    src={logo.image.filename}
+                    alt="partner logo"
+                    width={getSbImageDimensions("width", logo.image.filename)}
+                    height={getSbImageDimensions("height", logo.image.filename)}
+                  />
+                </div>
+              </Link>
+            )
+          })}
+        </div>
       </div>
     </section>
   )
