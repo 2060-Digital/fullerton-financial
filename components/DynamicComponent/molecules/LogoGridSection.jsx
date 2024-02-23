9e043c264c1c9dd6cd598feb7e244fd4217f7100
Ben McGrath <brmc14@gmail.com>
2024-02-23 16:32:43 -0500

    only center heading text on lg screens

--- components/DynamicComponent/molecules/LogoGridSection.jsx
+++ components/DynamicComponent/molecules/LogoGridSection.jsx
@@ -8,7 +8,7 @@ export default function LogoGridSection({ blok }) {
   return (
     <section className="px-6 py-12 lg:py-12">
       <div className="max-w-screen-xl mx-auto">
-        <h2 className="text-center">
+        <h2 className="lg:text-center">
           {blok?.eyebrow ? <span className="text-primary-1 block eyebrow pb-2.5">{blok?.eyebrow}</span> : null}
           <span className="pb-5 block text-primary-1">{blok?.heading}</span>
         </h2>
