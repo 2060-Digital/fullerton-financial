f0bd9528ad98d1899ef476f296ebd3c3ca57fbcc
Ben McGrath <brmc14@gmail.com>
2024-02-26 17:01:26 -0500

    mobile layout, add email and linkedin link styles

--- pages/team/[member].js
+++ pages/team/[member].js
@@ -1,9 +1,13 @@
 import Image from "next/image"
 import { getGlobals } from "storyblok/api"
 import { getTeamMember, getTeamMemberPaths } from "storyblok/teamMembers"
+import CallToAction from "components/CallToAction"
 import Meta from "components/Meta"
 import DynamicComponent from "components/DynamicComponent"
 import StoryblokVisualEditor from "components/StoryblokVisualEditor"
+import getSbImageDimensions from "utilities/getSbImageDimensions"
+import richText from "utilities/richText"
+import { getStoryblokLink } from "utilities/getStoryblokLink"
 
 export default function TeamMember({ story, meta }) {
   return (
@@ -11,14 +15,41 @@ export default function TeamMember({ story, meta }) {
       <Meta info={meta} />
       <StoryblokVisualEditor story={story?.content}>
         <main>
-          <section className="bg-primary-1">
+          <section className="bg-primary-1 px-6 py-11 lg:py-20">
             <div className="max-w-screen-xl mx-auto">
-              <div className="flex flex-col lg:flex-row">
-                <div>
-                  <Image src={story.content.image.filename} alt={story.content.image.alt} />
+              <div className="flex flex-col lg:flex-row gap-8">
+                <div className="lg:basis-1/3">
+                  <Image
+                    src={story.content.image.filename}
+                    alt={story.content.image.alt}
+                    height={getSbImageDimensions("height", story.content.image.filename)}
+                    width={getSbImageDimensions("width", story.content.image.filename)}
+                    className="aspect-square"
+                    style={{ width: "100%", height: "auto" }}
+                  />
                 </div>
-                <div className=""></div>
+                <div className="lg:basis-2/3">
+                  <h1 className="text-white pb-6">
+                    {story.content.first_name} {story.content.last_name}
+                  </h1>
+                  <div className="eyebrow text-white">{story.content.job_title}</div>
+                  <div className="py-4 flex flex-col gap-4">
+                    <CallToAction href={getStoryblokLink(story?.content.email)} style="email" target="_blank">
+                      {story.content.email.url}
+                    </CallToAction>
+                    <CallToAction href={getStoryblokLink(story?.content.linked_in)} style="linkedin">
+                      LINKEDIN PROFILE
+                    </CallToAction>
+                  </div>
+                  <div className="prose-p:text-white">{richText(story.content.intro_content)}</div>
+                </div>
               </div>
+            </div>
+          </section>
+          <section className="px-6 py-11 lg:py-20">
+            <div className="mx-auto max-w-screen-xl">
+              <div className="prose-headings:text-primary-1">{richText(story.content.sidebar_content)}</div>
+              <div>{richText(story.content.content)}</div>
             </div>
           </section>
 
