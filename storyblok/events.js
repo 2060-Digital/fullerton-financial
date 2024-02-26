ac612c6fbfa6422c48120525fdf41d0abb55d02e
Abel Marin <abel.marin@2060digital.com>
2024-02-26 16:16:12 -0500

    Remove unused import

--- storyblok/events.js
+++ storyblok/events.js
@@ -1,6 +1,5 @@
 import query from "storyblok/fetch"
 import EventArchive from "storyblok/gql/events/EventArchive.gql"
-import WebinarsArchive from "storyblok/gql/webinars/WebinarsArchive.gql"
 
 export async function getEventArchive(preview) {
   const data = await query(EventArchive, { preview })
