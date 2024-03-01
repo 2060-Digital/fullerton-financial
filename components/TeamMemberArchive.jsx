43903e4f44974ffd054eed5216a7263d416946f9
Ben McGrath <brmc14@gmail.com>
2024-03-01 17:51:20 -0500

    wrap link around team member image if vip or advisor

--- components/TeamMemberArchive.jsx
+++ components/TeamMemberArchive.jsx
@@ -8,29 +8,45 @@ import { getStoryblokLink } from "utilities/getStoryblokLink"
 import CallToAction from "components/CallToAction"
 import Arrow from "public/assets/chevron-down.svg"
 
-const Card = ({ image, first_name, last_name, job_title, email, vip, slug }) => {
+const Card = ({ image, first_name, last_name, job_title, team_category, email, vip, slug }) => {
   return (
     <div className={cn("bg-gray-light h-full", { "bg-secondary-2": vip })}>
       <div className="w-full relative">
         <div className="w-full h-full absolute border-2 border-secondary-1 top-5 right-5 z-10"></div>
-        <Image
-          loader={image.filename && image.filename !== "" ? storyblokImageLoader : undefined}
-          src={image.filename && image.filename !== "" ? image.filename : "/assets/placeholder.png"}
-          alt={image.alt ?? ""}
-          placeholder={image.blurDataURL ? "blur" : "empty"}
-          blurDataURL={image.blurDataURL}
-          width={getSbImageDimensions("width", image.filename)}
-          height={getSbImageDimensions("height", image.filename)}
-          className="w-full aspect-square object-cover min-h-[170px] relative z-20"
-          sizes="(max-width: 1024px) 45vw, (max-width: 640px) 95vw, 20vw"
-        />
+        {vip || team_category.includes("advisors") ? (
+          <Link href={slug} passHref>
+            <Image
+              loader={image.filename && image.filename !== "" ? storyblokImageLoader : undefined}
+              src={image.filename && image.filename !== "" ? image.filename : "/assets/placeholder.png"}
+              alt={image.alt ?? ""}
+              placeholder={image.blurDataURL ? "blur" : "empty"}
+              blurDataURL={image.blurDataURL}
+              width={getSbImageDimensions("width", image.filename)}
+              height={getSbImageDimensions("height", image.filename)}
+              className="w-full aspect-square object-cover min-h-[170px] relative z-20"
+              sizes="(max-width: 1024px) 45vw, (max-width: 640px) 95vw, 20vw"
+            />
+          </Link>
+        ) : (
+          <Image
+            loader={image.filename && image.filename !== "" ? storyblokImageLoader : undefined}
+            src={image.filename && image.filename !== "" ? image.filename : "/assets/placeholder.png"}
+            alt={image.alt ?? ""}
+            placeholder={image.blurDataURL ? "blur" : "empty"}
+            blurDataURL={image.blurDataURL}
+            width={getSbImageDimensions("width", image.filename)}
+            height={getSbImageDimensions("height", image.filename)}
+            className="w-full aspect-square object-cover min-h-[170px] relative z-20"
+            sizes="(max-width: 1024px) 45vw, (max-width: 640px) 95vw, 20vw"
+          />
+        )}
       </div>
       <div className="px-7 py-10">
         <h3 className="text-primary-1 pb-2">
           {first_name} {last_name}
         </h3>
         <div className={cn(" leading-7", { "text-primary-1": vip })}>{job_title}</div>
-        <CallToAction href={getStoryblokLink(email)} style="email-blue" target="_blank">
+        <CallToAction href={getStoryblokLink(email)} style="email-blue" target="_blank" className="z-20">
           {email?.url}
         </CallToAction>
       </div>
