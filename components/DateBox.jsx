150e36d35e1f2895070ec2cfc2041baf7655724a
Abel Marin <abel.marin@2060digital.com>
2024-03-01 13:10:20 -0700

    Make date box a dumb component

--- components/DateBox.jsx
+++ components/DateBox.jsx
@@ -1,10 +1,8 @@
-export default function DateBox({ date }) {
-  return (
+export default function DateBox({ month, day }) {
+  return month && day ? (
     <div className="bg-secondary-1 mb-4 lg:mb-0 py-4 px-6 lg:py-8 lg:px-10 text-center aspect-square">
-      <div className="eyebrow text-white">
-        {new Intl.DateTimeFormat("en-US", { month: "short" }).format(new Date(date))}
-      </div>
-      <h3 className="text-white">{new Intl.DateTimeFormat("en-US", { day: "2-digit" }).format(new Date(date))}</h3>
+      <div className="eyebrow text-white">{month}</div>
+      <h3 className="text-white">{day}</h3>
     </div>
-  )
+  ) : null
 }
