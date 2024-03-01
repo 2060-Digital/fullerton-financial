0d0124f35680c7000ae1993210f7d13b9124de64
Abel Marin <abel.marin@2060digital.com>
2024-03-01 14:25:59 -0600

    Fix resource card cta positioning

--- components/DynamicComponent/atoms/ResourceCard.jsx
+++ components/DynamicComponent/atoms/ResourceCard.jsx
@@ -5,7 +5,7 @@ import richText from "utilities/richText"
 
 export default function ResourceCard({ blok }) {
   return (
-    <div className="bg-secondary-2 px-7 py-10">
+    <div className="bg-secondary-2 px-7 py-10 flex flex-col">
       <Image
         src={blok?.thumbnail?.filename}
         alt={blok?.thumbnail?.alt ?? ""}
@@ -16,14 +16,14 @@ export default function ResourceCard({ blok }) {
         className="w-full pb-4"
         sizes="(max-width: 1024px) 45vw, (max-width: 640px) 95vw, 20vw"
       />
-      <div className="flex flex-col gap-4 items-center justify-between h-max">
-        <div>
-          <h3 className="text-center text-primary-1 pb-4">{blok?.heading}</h3>
-          <div className="prose-p:text-primary-1 text-center">{richText(blok?.content)}</div>
+      <div className="flex flex-col gap-4 items-center justify-between h-full">
+        <h3 className="text-center text-primary-1 pb-4">{blok?.heading}</h3>
+        <div className="prose-p:text-primary-1 text-center">{richText(blok?.content)}</div>
+        <div className="flex items-end grow">
+          <CallToAction href={getStoryblokLink(blok?.cta_link)} style="ghost">
+            {blok?.cta_label}
+          </CallToAction>
         </div>
-        <CallToAction href={getStoryblokLink(blok?.cta_link)} style="ghost" className="self-end">
-          {blok?.cta_label}
-        </CallToAction>
       </div>
     </div>
   )
