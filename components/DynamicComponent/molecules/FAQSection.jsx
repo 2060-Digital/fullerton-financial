52a99f32db233459d51a8dc3359872766b192cee
Abel Marin <abel.marin@2060digital.com>
2024-03-04 15:01:07 -0500

    Finish styling faq section

--- components/DynamicComponent/molecules/FAQSection.jsx
+++ components/DynamicComponent/molecules/FAQSection.jsx
@@ -7,26 +7,22 @@ const Item = ({ title, content }) => {
   const [expanded, setExpanded] = useState(false)
 
   return (
-    <div className="mx-auto py-8 w-full bg-secondary-2">
-      <div className="flex flex-row items-center cursor-pointer" onClick={() => setExpanded(!expanded)}>
-        <h3 className="grow !pb-0">{title}</h3>
-        <button onClick={() => setExpanded(!expanded)} className="shrink" aria-label="toggle accordion state">
-          <span
-            className={cn("text-black hover:text-green w-[85px]", {
-              "transition rotate-45 duration-300": expanded,
-              "transition rotate-90 duration-300": !expanded,
-            })}
-          >
-            <Expand />
-          </span>
+    <div className="mx-auto w-full bg-secondary-2 p-5 lg:p-7">
+      <div className="flex flex-row items-center gap-5 cursor-pointer" onClick={() => setExpanded(!expanded)}>
+        <h3 className="grow text-primary-1 !pb-0">{title}</h3>
+        <button
+          onClick={() => setExpanded(!expanded)}
+          className="accordion-button shrink"
+          aria-label="toggle accordion state"
+        >
+          <Expand className={cn({ expanded })} />
         </button>
       </div>
       <div
-        className={cn("leading-loose max-w-fit transition-transform origin-top break-words", {
+        className={cn("leading-loose max-w-fit transition-transform origin-top break-words lg:pr-20", {
           "max-h-0 scale-y-0 opacity-0": !expanded,
-          "max-h-full scale-y-1 opacity-1": expanded,
+          "max-h-full scale-y-1 opacity-1 pt-2": expanded,
         })}
-        inert={!expanded ? "" : null}
       >
         {richText(content)}
       </div>
@@ -36,10 +32,12 @@ const Item = ({ title, content }) => {
 
 export default function FAQSection({ blok }) {
   return (
-    <section className="py-8 relative">
-      <h2 className="pb-12">{blok.heading}</h2>
-      <div className="mx-auto max-w-screen-lg px-4 lg:px-0">
-        {blok.items ? blok.items.map((item) => <Item {...item} key={item._uid} />) : null}
+    <section className="faq-section py-8 relative">
+      <h2 className="mb-6">{blok.heading}</h2>
+      <div className="mx-auto max-w-screen-lg flex flex-col gap-8">
+        {blok?.items?.map((item) => (
+          <Item {...item} key={item?._uid} />
+        ))}
       </div>
     </section>
   )
