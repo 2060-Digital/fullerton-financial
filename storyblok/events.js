7652250d066acea78e71132bac7afe30da594ab3
Abel Marin <abel.marin@2060digital.com>
2024-03-05 14:33:28 -0500

    Fix get event archive function

--- storyblok/events.js
+++ storyblok/events.js
@@ -2,7 +2,7 @@ import query from "storyblok/fetch"
 import EventArchive from "storyblok/gql/events/EventArchive.gql"
 
 export async function getEventArchive(slug, preview) {
-  const data = await query(EventArchive, { preview, slug })
+  const data = await query(EventArchive, { variables: { slug }, preview })
 
   return data?.EventsarchiveItem
 }
