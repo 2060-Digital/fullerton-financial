ab4b353ce48eaa1b2a1405bb9bdaf185eadfbf6d
abelmarin <52367640+abelmarin@users.noreply.github.com>
2024-03-05 15:34:33 -0500

    Update homepage components (#102)
    * Update video hero styles
    * Update logo carousel styles
    * prevent header from causes preview pane crashes
    * Fix padding/margin on two column section

--- components/DynamicComponent/molecules/TwoColumnSection.jsx
+++ components/DynamicComponent/molecules/TwoColumnSection.jsx
@@ -4,7 +4,7 @@ import richText from "utilities/richText"
 
 export default function TwoColumnSection({ blok }) {
   return (
-    <section className="py-12 md:py-24 px-6">
+    <section className="py-12 md:py-20 px-6">
       <div className="max-w-screen-lg mx-auto">
         <div className="mx-auto pb-8 max-w-screen-md">
           <h2 className="text-center">
@@ -14,7 +14,7 @@ export default function TwoColumnSection({ blok }) {
           <div className="md:text-center">{richText(blok?.intro_content)}</div>
         </div>
         <div
-          className={cn("flex flex-col gap-8", {
+          className={cn("flex flex-col gap-8 ", {
             "md:flex-row-reverse": blok?.orientation === "content_first",
             "md:flex-row": blok?.orientation === "image_first",
           })}
@@ -30,7 +30,7 @@ export default function TwoColumnSection({ blok }) {
               className="w-full relative -top-4 -right-4"
             />
           </div>
-          <div className="basis-1/2 prose-a:mb-10 prose-headings:text-primary-1 prose-headings:mb-4">
+          <div className="basis-1/2 prose-a:mb-10 prose-headings:text-primary-1 prose-headings:mb-4 last:prose-a:mb-0">
             {richText(blok?.content)}
           </div>
         </div>
