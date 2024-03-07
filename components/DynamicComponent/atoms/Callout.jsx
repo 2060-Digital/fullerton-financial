8925d827c544dd150db2744d14c9d3d4da1ff991
Abel Marin <abel.marin@2060digital.com>
2024-03-07 15:17:01 -0500

    Add missing padding

--- components/DynamicComponent/atoms/Callout.jsx
+++ components/DynamicComponent/atoms/Callout.jsx
@@ -1,6 +1,6 @@
 export default function Callout({ blok }) {
   return (
-    <section className="my-12 lg:my-24">
+    <section className="my-12 lg:my-24 px-6">
       <div className="p-7 bg-secondary-2 max-w-screen-xl mx-auto">
         <blockquote className="font-secondary text-primary-1 font-medium leading-7 lg:text-center text-m2 max-w-3xl mx-auto">
           {blok?.quote}
