6f913110f23c8a24642345c5674ae280991d6de8
Ben McGrath <brmc14@gmail.com>
2024-03-11 11:07:36 -0400

    add and style list item counter in mobile and desktop

--- components/DynamicComponent/molecules/TimelineSection.jsx
+++ components/DynamicComponent/molecules/TimelineSection.jsx
@@ -4,7 +4,7 @@ import richText from "utilities/richText"
 
 function TimelineEntry({ image, heading, content }) {
   return (
-    <li className="group odd:flex-row-reverse lg:flex border-l-2 border-white last:border-none lg:border-none">
+    <li className="group odd:flex-row-reverse lg:flex border-l-2 border-white last:border-none lg:border-none lg:relative">
       <div className="image-decor relative z-10 basis-1/2 lg:max-h-[500px] lg:min-h-[336px] lg:pb-24">
         <div className="relative z-10 border-2 border-l-0 lg:border-l-2 border-white lg:group-even:border-r-0 lg:group-odd:border-l-0 lg:group-last-of-type:border-r-2 group-last:border-l-2 lg:group-last:left-[2px]">
           <Image
@@ -18,8 +18,8 @@ function TimelineEntry({ image, heading, content }) {
           />
         </div>
       </div>
-      <div className="basis-1/2 pb-16 pt-4 lg:py-8 border-white lg:group-odd:border-r-2 lg:group-even:border-l-2 lg:border-t-2 lg:-mr-[2px] lg:group-last-of-type:border-l-0">
-        <div className=" border-white pb-4 pl-4 lg:group-even:pl-8 lg:group-odd:pl-0 lg:group-odd:pr-8">
+      <div className="basis-1/2 pb-16 pt-4 lg:py-8 border-white lg:group-odd:border-r-2 lg:group-even:border-l-2 lg:border-t-2 lg:-mr-[2px] lg:group-last-of-type:border-l-0 lg:group-even:text-right">
+        <div className="timeline-content border-white pl-4 lg:group-even:pl-8 lg:group-odd:pl-0 lg:group-odd:pr-8 relative lg:static">
           <h3 className="block text-primary-1">{heading}</h3>
         </div>
         <div className="pt-4 pl-4 lg:group-even:pl-8 lg:group-odd:pl-0 lg:group-odd:pr-8">{richText(content)}</div>
@@ -30,12 +30,12 @@ function TimelineEntry({ image, heading, content }) {
 
 export default function TimelineSection({ blok }) {
   return (
-    <section className="px-6 py-12 timeline">
-      <div className="text-center py-16">
+    <section className="px-6 py-12 lg:py-24 timeline">
+      <div className="text-center pb-16 relative">
         <div className="text-primary-1 eyebrow">{blok.eyebrow}</div>
         <h2 className="text-primary-1">{blok.heading}</h2>
       </div>
-      <ol className="mx-auto max-w-[1070px]">
+      <ol className="timeline-list mx-auto max-w-[1070px]">
         {blok.entries.map((entry) => (
           <TimelineEntry {...entry} key={entry.heading} />
         ))}
