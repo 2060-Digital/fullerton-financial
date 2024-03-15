88ccfbb5a4e019d2eb894e6853a94cfe653923d0
Ben McGrath <brmc14@gmail.com>
2024-03-15 16:25:45 -0400

    adjust padding/margins on EventsCarousel, ImageAndeContent, ThreeColumnSection

--- components/DynamicComponent/molecules/ThreeColumnSection.jsx
+++ components/DynamicComponent/molecules/ThreeColumnSection.jsx
@@ -4,10 +4,10 @@ import isRichTextValid from "utilities/isRichTextValid"
 
 export default function ThreeColumnSection({ blok }) {
   return (
-    <section className="px-5 py-12 lg:py-20">
+    <section className="px-5 pb-12 lg:pb-20">
       <div className="three-column-section mx-auto">
         {blok?.eyebrow || blok?.heading || isRichTextValid(blok?.content) ? (
-          <div className="mx-auto mb-12 max-w-screen-md lg:mb-14">
+          <div className="mx-auto mb-6 max-w-screen-md lg:mb-12">
             {blok?.eyebrow ? <div className="eyebrow pb-3 text-center text-primary-1">{blok.eyebrow}</div> : null}
             {blok?.heading ? <h2 className="pb-3 text-center text-primary-1">{blok.heading}</h2> : null}
             <div className="text-center">{richText(blok.content)}</div>
