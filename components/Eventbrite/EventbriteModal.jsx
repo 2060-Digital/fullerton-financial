440f76b359cde2ff214f26db3e20896d8cddd60a
Ben McGrath <brmc14@gmail.com>
2024-03-27 11:08:34 -0400

    adjust height of eventbrite modal on vertically shorter screens

--- components/Eventbrite/EventbriteModal.jsx
+++ components/Eventbrite/EventbriteModal.jsx
@@ -14,7 +14,7 @@ export default function Modal({ eventHash, isOpen, closeModal, focusRef }) {
         tabIndex={0}
         ref={focusRef}
         className={cn(
-          "modal fixed left-0 top-0 z-[9999] h-[100dvh] w-full max-w-screen-lg transform overflow-y-auto bg-white sm:left-1/2 sm:top-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 lg:h-auto",
+          "modal fixed left-0 top-0 z-[9999] h-[100dvh] w-full max-w-screen-lg transform overflow-y-auto bg-white sm:left-1/2 sm:top-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 lg:h-auto lg:max-h-[80dvh]",
           {
             hidden: !isOpen,
           },
