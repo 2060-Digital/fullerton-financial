d7b4a74c903fc47332687e11a5b1d7c696bcd330
Ben McGrath <brmc14@gmail.com>
2024-03-29 14:33:44 -0400

    conditionally render images

--- components/Eventbrite/StructuredContentSection.jsx
+++ components/Eventbrite/StructuredContentSection.jsx
@@ -1,37 +1,40 @@
-import AccordionItem from "components/AccordionItem"
 import FAQSection from "components/DynamicComponent/molecules/FAQSection"
 import Image from "next/image"
 
 export default function StructuredContentSection({ modules, faqs }) {
+  const images = modules.filter((mod) => mod.type === "image")
+  const textBlocks = modules.filter((mod) => mod.type === "text")
+
   return modules?.length > 0 ? (
     <section className="px-6">
       <div className="mx-auto max-w-screen-xl py-12 lg:border-b-2 lg:border-b-secondary-1 lg:py-20 lg:pb-12">
         <div className="grid gap-12 lg:grid-cols-2">
-          {modules.map(({ data, id }) => {
-            if (data?.image) {
-              return (
-                <div className="relative z-10 order-first mr-4 mt-4 h-full w-full border-2 border-secondary-1" key={id}>
-                  <Image
-                    src={data?.image?.url}
-                    alt=""
-                    width={585}
-                    height={350}
-                    sizes="95vw, (min-width: 1024px) 35vw"
-                    className="relative -right-4 -top-4 h-full w-full object-cover"
-                  />
-                </div>
-              )
-            }
-            return (
+          {images.length > 0 ? (
+            <div className="relative z-10 order-first mr-4 mt-4 h-full w-full border-2 border-secondary-1">
+              {images.map(({ data, id }) => (
+                <Image
+                  key={id}
+                  src={data?.image?.url}
+                  alt=""
+                  width={585}
+                  height={350}
+                  sizes="95vw, (min-width: 1024px) 35vw"
+                  className="relative -right-4 -top-4 h-full w-full object-cover"
+                />
+              ))}
+            </div>
+          ) : null}
+          <div>
+            {textBlocks.map(({ data, id }) => (
               <div
                 dangerouslySetInnerHTML={{ __html: data?.body?.text ?? null }}
                 key={id}
                 className="event-page-content prose-headings:pb-4 prose-headings:text-primary-1 prose-h3:pt-2 prose-h3:text-l2 first:prose-h3:pt-0 prose-h4:font-secondary prose-h4:text-m2 prose-h3:lg:text-xl1 prose-h4:lg:text-l1"
               ></div>
-            )
-          })}
+            ))}
+          </div>
         </div>
-        {faqs.length > 0 ? <FAQSection blok={{ heading: "Frequently Asked Questions", items: faqs }} /> : null}
+        {faqs?.length > 0 ? <FAQSection blok={{ heading: "Frequently Asked Questions", items: faqs }} /> : null}
       </div>
     </section>
   ) : null
