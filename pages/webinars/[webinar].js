4a1ce340a0f349976cf8383fc00a276bd89f6f0c
Ben McGrath <brmc14@gmail.com>
2024-10-15 15:50:27 -0400

    comment out webinar code

--- pages/webinars/[webinar].js
+++ pages/webinars/[webinar].js
@@ -1,50 +1,50 @@
-import { getGlobals } from "storyblok/api"
-import { getIndividualWebinarPaths, getWebinarByIDs } from "go-to-webinar/api"
-import WebinarPage from "components/Webinars/WebinarPage"
-import Script from "next/script"
+// import { getGlobals } from "storyblok/api"
+// import { getIndividualWebinarPaths, getWebinarByIDs } from "go-to-webinar/api"
+// import WebinarPage from "components/Webinars/WebinarPage"
+// import Script from "next/script"
 
-export default function Webinar({ webinar }) {
-  return (
-    <>
-      <WebinarPage webinar={webinar} />
-      <Script type="application/ld+json" id="event-schema">
-        {`{
-        "@context": "https://schema.org",
-        "@type": "BusinessEvent",
-        "name": "${webinar?.subject}",
-        "location": {
-          "@type": "VirtualLocation",
-          "url": "https://www.goto.com/webinar"
-        },
-        "offers": "${webinar?.registrationUrl}",
-        "startDate": "${webinar?.times[0]?.startTime}",
-        "endDate": "${webinar?.times[0]?.endTime}",
-        ${webinar?.description?.length ? `"description": "${webinar.description}",` : ""}
-        "eventAttendanceMode": "https://schema.org/OnlineEventAttendanceMode"
-      }`}
-      </Script>
-    </>
-  )
-}
+// export default function Webinar({ webinar }) {
+//   return (
+//     <>
+//       <WebinarPage webinar={webinar} />
+//       <Script type="application/ld+json" id="event-schema">
+//         {`{
+//         "@context": "https://schema.org",
+//         "@type": "BusinessEvent",
+//         "name": "${webinar?.subject}",
+//         "location": {
+//           "@type": "VirtualLocation",
+//           "url": "https://www.goto.com/webinar"
+//         },
+//         "offers": "${webinar?.registrationUrl}",
+//         "startDate": "${webinar?.times[0]?.startTime}",
+//         "endDate": "${webinar?.times[0]?.endTime}",
+//         ${webinar?.description?.length ? `"description": "${webinar.description}",` : ""}
+//         "eventAttendanceMode": "https://schema.org/OnlineEventAttendanceMode"
+//       }`}
+//       </Script>
+//     </>
+//   )
+// }
 
-export async function getStaticProps({ params: { webinar } }) {
-  const globals = await getGlobals()
+// export async function getStaticProps({ params: { webinar } }) {
+//   const globals = await getGlobals()
 
-  const webinarIDs = webinar.split("-")
+//   const webinarIDs = webinar.split("-")
 
-  const individualWebinar = await getWebinarByIDs(webinarIDs[webinarIDs.length - 2], webinarIDs[webinarIDs.length - 1])
+//   const individualWebinar = await getWebinarByIDs(webinarIDs[webinarIDs.length - 2], webinarIDs[webinarIDs.length - 1])
 
-  return {
-    props: {
-      globals,
-      webinar: individualWebinar ?? null,
-    },
-  }
-}
+//   return {
+//     props: {
+//       globals,
+//       webinar: individualWebinar ?? null,
+//     },
+//   }
+// }
 
-export async function getStaticPaths() {
-  return {
-    paths: await getIndividualWebinarPaths(),
-    fallback: false,
-  }
-}
+// export async function getStaticPaths() {
+//   return {
+//     paths: await getIndividualWebinarPaths(),
+//     fallback: false,
+//   }
+// }
