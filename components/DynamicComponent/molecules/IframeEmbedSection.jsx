e1987335978dc49b87f32141c14ae41a8a128ffe
Ben McGrath <brmc14@gmail.com>
2024-11-12 11:03:56 -0500

    add IframeEmbedSection

---
+++ components/DynamicComponent/molecules/IframeEmbedSection.jsx
@@ -0,0 +1,12 @@
+import cn from "classnames"
+import richText from "utilities/richText"
+
+export default function IframeEmbedSection({ blok }) {
+  return (
+    <section className={`px-5 py-8 lg:py-16`}>
+      <div className="mx-auto max-w-screen-xl">
+        <iframe src={blok?.src?.url} height={blok.initial_height} className="w-full" />
+      </div>
+    </section>
+  )
+}
